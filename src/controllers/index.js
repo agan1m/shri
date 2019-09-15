5f2b9565ea60ff475a22e32fd9dff6aa8446248c
user <user@MacBook-Air-user.local>
2019-09-15 14:01:29 +0300

    add routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { exec } = require('child_process');
 const rmDir = require('../utils/rmdirUtil');
 
 exports.getReposList = (req, res) => {
@@ -22,3 +23,68 @@ exports.deleteRepoById = (req, res) => {
 		res.json({ data: {}, isSuccess: true });
 	});
 };
+
+exports.getCommits = (req, res) => {
+	const { repositoryId, commitHash = 'master' } = req.params;
+	
+	exec(`git log ${commitHash} --pretty=format:"%H-%an-%ad"`, {
+		cwd: path.join(global.reposPath, repositoryId)
+	}, (err, stdout) => {
+		if(err) {
+			throw err;
+		}
+		console.log(stdout)
+		const data = stdout.split('\n').map(commit => {
+			const parse = commit.split('-');
+			return {
+				hash: parse[0],
+				author: parse[1],
+				date: parse[2],
+			}
+		});
+		res.json({data})
+	})
+};
+
+exports.getDiffCommit = (req, res) => {
+	const {repositoryId, commitHash} = req.params;
+
+	exec(`git diff ${commitHash}^ --stat`, {
+		cwd: path.join(global.reposPath, repositoryId),
+	}, (err, stdout) => {
+		if(err) {
+			throw err;
+		}
+		res.json({
+			data: stdout
+		})
+	})
+};
+
+exports.getTree = (req, res) => {
+	const {repositoryId, path: pathParam, commitHash} = req.params;
+	
+	exec(`git ls-tree --full-tree ${commitHash || 'master'} ${pathParam || ''}`, {
+		cwd: path.join(global.reposPath, repositoryId),
+	}, (err, stdout) => {
+		if(err) {
+			throw err;
+		}
+
+		res.json({data: stdout})
+	});
+};
+
+exports.getFileContent = (req, res) => {
+	const {repositoryId, commitHash, pathToFile} = req.params;
+
+	exec(`git show ${commitHash}:${pathToFile}`, {
+		cwd: path.join(global.reposPath, repositoryId),
+	}, (err, stdout) => {
+		if(err) {
+			throw err;
+		}
+
+		res.json({data: stdout})
+	})
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getReposList, deleteRepoById } = require('../controllers');
+const { getReposList, deleteRepoById, getCommits, getDiffCommit, getTree, getFileContent } = require('../controllers');
 
 router.get('/repos', getReposList);
+
+router.get('/repos/:repositoryId/commits/:commitHash', getCommits);
+
+router.get('/repos/:repositoryId/commits/:commitHash/diff', getDiffCommit);
+
+router.get('/repos/:repositoryId', getTree);
+
+router.get('/repos/:repositoryId/tree/:commitHash?/:path([^/]*)', getTree);
+
+router.get('/repos/:repositoryId/blob/:commitHash/:pathToFile([^/]*)', getFileContent);
 
 router.delete('/repos/:repositoryId', deleteRepoById);
 
